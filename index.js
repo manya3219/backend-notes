8f160c61b405e27aee83b2ffad2c65a8965a6dac
manya3219 <yadavmanya1902@gmail.com>
2025-12-04 15:41:08 +0530

    third commit

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(express.static('public'));
 app.set("views",path.join(__dirname,"/views"));
 app.set('view engine' , 'ejs');
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://frontend-notes-2-ija5.onrender.com',
   credentials: true
 }));
 app.use(express.json());
