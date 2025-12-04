57c50fd1b2a8194f462648249f6174b430f4db3a
manya3219 <yadavmanya1902@gmail.com>
2025-12-05 00:14:35 +0530

    Fix: Return proper JSON response in signout endpoint

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -70,7 +70,7 @@ export const signout = (req, res, next) => {
     res
       .clearCookie('access_token')
       .status(200)
-      .json('User has been signed out');
+      .json({ success: true, message: 'User has been signed out' });
   } catch (error) {
     next(error);
   }
